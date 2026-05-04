7c3fd16fddb1b422de37283dc29f39114b363f3c
Yasir Azizi <yasir720@hotmail.com>
2026-05-04 12:45:38 -0400

    Added in Diana as a customer

--- setup/sample_data.sql
+++ setup/sample_data.sql
@@ -2,6 +2,7 @@ INSERT INTO customers VALUES (1, 'Yasir', 'yasir@wvb.com');
 INSERT INTO customers VALUES (2, 'Brian', 'brian@wvb.com');
 INSERT INTO customers VALUES (3, 'Cesar', 'cesar@wvb.com');
 INSERT INTO customers VALUES (4, 'Crystal', 'crystal@wvb.com');
+INSERT INTO customers VALUES (5, 'Diana', 'diana@wvb.com');
 
 
 INSERT INTO menu_items VALUES (1, 'Coffee', 3.50);
